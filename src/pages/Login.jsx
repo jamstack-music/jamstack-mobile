a434d175f6477d64b47681a8259d9baadfafcb12
Zach Banducci <zbducci@gmail.com>
2019-03-25 17:34:28 -0500

    Changed amount of necc spaces in eslint config

---
+++ src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { Text, View, StyleSheet } from 'react-native'
+
+type Props = {}
+export default class Login extends Component<Props> {
+  render() {
+    return (
+      <View>
+        <Text>Hello</Text>
+      </View>
+    )
+  }
+}
