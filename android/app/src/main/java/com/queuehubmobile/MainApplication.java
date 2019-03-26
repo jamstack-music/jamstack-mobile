c542027132f3e002f08d8373f11a32bce770f352
Zach Banducci <zbducci@gmail.com>
2019-03-25 20:19:40 -0500

    Added spotify sdk integration for ios TODO: android needed

--- android/app/src/main/java/com/queuehubmobile/MainApplication.java
+++ android/app/src/main/java/com/queuehubmobile/MainApplication.java
@@ -3,6 +3,8 @@ package com.queuehubmobile;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.spotify.sdk.android.authentication.RNSpotifyPackage;
+import com.lufinkey.react.eventemitter.RNEventEmitterPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNSpotifyPackage(),
+            new RNEventEmitterPackage(),
             new RNGestureHandlerPackage()
       );
     }
