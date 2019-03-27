72272f7052b4d7133b90a953c749695a0b2c5981
Zach Banducci <zbducci@gmail.com>
2019-03-27 01:07:43 -0500

    Added Spotify initialization and login

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,14 +1,29 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import React from 'react'
+import { View, Button, Alert } from 'react-native'
+import Spotify from 'rn-spotify-sdk'
 
-export default class Login extends Component {
-  render() {
-    return (
-      <View>
-        <Text>Hello</Text>
-        <Text>Hello</Text>
-        <Text>Hello</Text>
-      </View>
-    )
+const Login = ({navigation}) => {
+  const initializeSpotify = async () => {
+    if(!Spotify.isInitialized()) {
+      await Spotify.initialize({
+        'clientID':'0a31a2abfc5945bb9e3b3507e6f8361c',
+        'sessionUserDefaultsKey':'SpotifySession',
+        'redirectURL':'queuehubmobile://auth'
+      })
+    }
+    let loggedIn = await Spotify.login()
+
+    if(loggedIn) {
+      navigation.navigate('Home')
+    } else {
+      Alert('Whoops look like something bad happened')
+    } 
   }
+  return (
+    <View>
+      <Button title="Login with Spotify" onPress={() => initializeSpotify()}/>
+    </View>
+  )
 }
+
+export default Login
