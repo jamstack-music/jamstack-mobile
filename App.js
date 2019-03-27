96a6cb83ee9fd0598b0a5bfc1f558f215b96021d
Zach Banducci <zbducci@gmail.com>
2019-03-27 02:54:33 -0500

    Added spotify sdk init to app initial load

--- App.js
+++ App.js
@@ -1,5 +1,13 @@
 import React from 'react'
 import AppContainer from './src/navigation/AppNavigator'
+import Spotify from 'rn-spotify-sdk'
+
+Spotify.initialize({
+  'clientID':'0a31a2abfc5945bb9e3b3507e6f8361c',
+  'sessionUserDefaultsKey':'SpotifySession',
+  'redirectURL':'queuehubmobile://auth',
+  'scopes': ["streaming"]
+})
 
 const App = () => <AppContainer />
 
