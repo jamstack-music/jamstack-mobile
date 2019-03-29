1272cb1f3b0b64e31d8fc02772fb2f922dd3c10a
Zach Banducci <zbducci@gmail.com>
2019-03-28 20:24:55 -0500

    Added timer hook that allows you to start and stop a timer

---
+++ src/hooks/useTimer.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react'
+
+// Custom timer hook that will keep track of the time elapsed from an initial start point
+function useTimer(startTime = 0) {
+  let timerRef = null
+  const [timer, setTimer] = useState(startTime)
+  useEffect(() => {
+    timerRef = setInterval(() => tick(), 1000)
+    return function(){
+      clearInterval(timerRef)
+    }
+  })
+
+  const tick = () => setTimer(timer + 1)
+  const toggleTimer = () => clearInterval(timerRef)
+  return [timer, toggleTimer] 
+}
+
+export default useTimer
