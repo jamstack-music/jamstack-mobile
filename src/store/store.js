e576e90a7e75488976e1f8516ac572f807b5caf0
Zach Banducci <zbducci@gmail.com>
2019-03-30 13:58:36 -0500

    Added next song and add song functionality to store

--- src/store/store.js
+++ src/store/store.js
@@ -8,4 +8,23 @@ export default class RoomContainer extends Container {
     public: true,
     password: ''
   }
+
+  addToQueue = song => this.setState(prevState => {
+    prevState
+  })
+
+  nextSong = () => {
+    if(this.state.queue.length > 0) {
+      this.setState(prevState => {
+        currentSong: prevState.queue[prevState.queue.length - 1], 
+        queue: prevState.queue.slice(0, prevState.queue.length - 1)
+      })
+    }
+  }
+
+  addSong = song => {
+    this.setState(prevState => {
+      queue: prevState.queue.insert(0, song)
+    })
+  } 
 } 
