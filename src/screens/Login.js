23fc9ba63f02f0fd360bf55265723a4e4e4d3153
Zach Banducci <zbducci@gmail.com>
2019-03-29 23:01:48 -0500

    Cleaned up Login page

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,32 +1,22 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { View, Button, Alert } from 'react-native'
 import Spotify from 'rn-spotify-sdk'
-import Player from '../components/Player'
 
 const Login = ({navigation}) => {
-  const [loggedin, setloggedin] = useState(false)
   const login = async () => {
     let loggedIn = await Spotify.login()
     if(loggedIn) {
-      setloggedin(true)
+      // Navigate to correct screen
     } else {
       Alert.alert('You gotta log in')
     }
   }
 
-  if(!loggedin) {
-    return ( 
-      <View>
-        <Button title="Login to Spotify" onPress={() => login()} />
-      </View>
-    )
-  } else {
-    return (
-      <View>
-        <Player nextSong={() => { console.log("skip") }} uri={"spotify:track:0GNOV2aEFqS3qOXfQEhEuq"} />
-      </View>
-    )
-  }
+  return ( 
+    <View>
+      <Button title="Login to Spotify" onPress={() => login()} />
+    </View>
+  )
 }
 
 export default Login
