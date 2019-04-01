bdea2a0f0c03e4b13106539bf4a483243eb6c848
Zach Banducci <zbducci@gmail.com>
2019-03-31 20:48:47 -0500

    Application navigator and auth middleware

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,10 +1,24 @@
-import { createAppContainer, createStackNavigator } from 'react-navigation'
+import { createAppContainer, createStackNavigator, createSwitchNavigator } from 'react-navigation'
 
 import Login from '../screens/Login'
+import Room from '../screens/Room'
+import Auth from '../navigation/Auth'
 
-const AppNavigator = createStackNavigator({
+const RoomStack = createStackNavigator({
+  Room
+})
+
+const LoginStack = createStackNavigator({
   Login
 })
-const AppContainer = createAppContainer(AppNavigator)
 
-export default AppContainer
+const Application = createSwitchNavigator({
+  Login: LoginStack, 
+  Room: RoomStack,
+  Auth
+}, {
+  initialRouteName: 'Auth'
+})
+
+export default createAppContainer(Application)
+
