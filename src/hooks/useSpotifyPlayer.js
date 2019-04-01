d24cebd14e1506022c9c18a3b26137aa75f091d3
Zach Banducci <zbducci@gmail.com>
2019-04-01 13:12:53 -0500

    Song seeker in useSpotifyPlayer instead of useTimer

--- src/hooks/useSpotifyPlayer.js
+++ src/hooks/useSpotifyPlayer.js
@@ -4,8 +4,11 @@ import Spotify from 'rn-spotify-sdk'
 // TODO: Validate that song being passed in is a Spotify URI
 // State hook for keeping playing songs
 function useSpotifyPlayer(uri) {
+  let timerRef = null
   // Play status
   const [play, setPlay] = useState(false)
+  // Playback status of song (in ms)
+  const [elapsed, setElapsed] = useState(0)
 
   useEffect(function playNewSong() {
     Spotify.playURI(uri, 0, 0)
@@ -16,7 +19,19 @@ function useSpotifyPlayer(uri) {
     Spotify.setPlaying(play) 
   }, [play])
 
-  return [play, setPlay]
+  useEffect(function songTimer() {
+    timerRef = setInterval(() => timeElapsed(), 100)
+    return function(){
+      clearInterval(timerRef)
+    }
+  })
+
+  const timeElapsed = () => {
+    const { position } = Spotify.getPlaybackState()
+    setElapsed(position * 1000)
+  }
+
+  return [play, setPlay, elapsed]
 }
 
 export default useSpotifyPlayer
