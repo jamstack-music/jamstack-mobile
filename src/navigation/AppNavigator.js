e814e369abecb08edd1c95dd9d614c0d54bd0bbd
Zach Banducci <zbducci@gmail.com>
2019-04-01 00:05:57 -0500

    Tab navigator and basic icons added

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,19 +1,52 @@
+import React from 'react'
+
 import { 
   createAppContainer, 
+  createStackNavigator,
   createSwitchNavigator,
   createBottomTabNavigator
 } from 'react-navigation'
 
+import Ionicons from 'react-native-vector-icons/Ionicons'
+
 import Login from '../screens/Login'
 import Room from '../screens/Room'
 import Auth from '../navigation/Auth'
+import Search from '../screens/Search'
+import Browse from '../screens/Browse'
 
 const RoomStack = createBottomTabNavigator({
-  Room
+  Search,
+  Room,
+  Browse
+}, {
+  defaultNavigationOptions: ({navigation}) => ({
+    tabBarIcon: ({tintColor}) => {
+      const { routeName } = navigation.state
+      let iconName
+      switch (routeName) {
+      case 'Browse': 
+        iconName = 'md-disc'
+        break
+      case 'Search':
+        iconName = 'md-search'
+        break
+      case 'Room':
+        iconName = 'md-home'
+        break
+      }
+
+      return <Ionicons name={iconName} size={20} color={tintColor}/>
+    }
+  }),
+  tabBarOptions: {
+    activeTintColor: 'tomato',
+    inactiveTintColor: 'grey'
+  }
 })
 
 const Application = createSwitchNavigator({
-  Login,
+  Login: createStackNavigator({Login}),
   Room: RoomStack,
   Auth
 }, {
