0809b22f027ec35156cdcdbf9a5d5b6cd42f2129
Zach Banducci <zbducci@gmail.com>
2019-04-03 16:22:32 -0500

    Room screen added and basic share functionality started

--- src/screens/Room.js
+++ src/screens/Room.js
@@ -1,16 +1,50 @@
 import React from 'react'
-import { View } from 'react-native'
+import { Share, Button, View, Text, StyleSheet } from 'react-native'
 
 import { Subscribe } from 'unstated'
-import SongPlayer from '../components/SongPlayer'
 import RoomContainer from '../store/store'
 
-const Room = () => (
-  <Subscribe to={[RoomContainer]}>
-    {room => (
-      <SongPlayer {...room.state.currentSong} nextSong={() => room.nextSong()}/>  
-    )}
-  </Subscribe>
-)
+const Room = () => { 
+  const shareRoom = async (roomName) => {
+    const result = await Share.share({
+      message: `Join ${roomName} on Queuehub!`,
+      url: 'https://google.com'
+    }) 
+  }
+
+  return (
+    <Subscribe to={[RoomContainer]}>
+      { room => (
+        <View style={styles.container}>
+          <Text style={styles.title}>Placeholder room title</Text>
+          <Button 
+            title='Share Room Code'
+            onPress={() => shareRoom(room.state.name)} />
+          <View>
+            <Text>Total Members</Text>
+            <Text>{room.state.members.length}</Text>
+          </View>
+          <View>
+            <Text>Queue</Text>
+            {/* INSERT SONG LIST ITEM HERE */}
+          </View>
+        </View>
+      )}
+    </Subscribe>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold'
+  }
+})
 
 export default Room
