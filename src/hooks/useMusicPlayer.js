b3a0935f5df8a2d60d566c1b8c572969e556a595
Zach Banducci <zbducci@gmail.com>
2019-04-04 16:18:56 -0500

    Cleaned up player hooks and added eventlistener for song completion

--- src/hooks/useMusicPlayer.js
+++ src/hooks/useMusicPlayer.js
@@ -1,11 +1,16 @@
 import { useEffect } from 'react'
+import PropTypes from 'prop-types'
 
 import MusicControl from 'react-native-music-control'
 import useSpotifyPlayer from './useSpotifyPlayer'
 
+/**
+ * useMusicPlayer: Generic music player hook that updates currently playing information
+ * @author [Zach Banducci](https://github.com/zchbndcc9)
+ */
 function useMusicPlayer({title, artist, album, uri, duration}, nextSong) {
-  const [play, setPlay, elapsed] = useSpotifyPlayer(uri)
-
+  const [play, setPlay, elapsed] = useSpotifyPlayer(uri, nextSong) 
+  
   useEffect(function updatePlayback() {
     MusicControl.enableBackgroundMode(true)
 
@@ -30,4 +35,8 @@ function useMusicPlayer({title, artist, album, uri, duration}, nextSong) {
   return [play, setPlay, elapsed]
 }
 
+useMusicPlayer.propTypes = {
+  song: PropTypes.object.isRequired,
+  nextSong: PropTypes.func.isRequired
+}
 export default useMusicPlayer
--- src/hooks/useSpotifyPlayer.js
+++ src/hooks/useSpotifyPlayer.js
@@ -1,14 +1,21 @@
 import { useState, useEffect } from 'react'
+import PropTypes from 'prop-types'
 import Spotify from 'rn-spotify-sdk'
-
-// TODO: Validate that song being passed in is a Spotify URI
-// State hook for keeping playing songs
-function useSpotifyPlayer(uri) {
+/**
+ * useSpotifyPlayer : State hook for playing songs and listening for when a song is over
+ * @author [Zach Banducci](https://github.com/zchbndcc9)
+ */
+function useSpotifyPlayer(uri, nextSong) {
   let timerRef = null
   // Play status
   const [play, setPlay] = useState(false)
   // Playback status of song (in ms)
   const [elapsed, setElapsed] = useState(0)
+
+  // Event listener to see if the track has finished and if it has it changes to the next song
+  useEffect(function init() {
+    Spotify.on('audioDeliveryDone', () => nextSong())
+  }, [])
 
   useEffect(function playNewSong() {
     Spotify.playURI(uri, 0, 0)
@@ -32,6 +39,11 @@ function useSpotifyPlayer(uri) {
   }
 
   return [play, setPlay, elapsed]
+}
+ 
+useSpotifyPlayer.propTypes = {
+  uri: PropTypes.string.isRequired,
+  nextSong: PropTypes.func.isRequired
 }
 
 export default useSpotifyPlayer
