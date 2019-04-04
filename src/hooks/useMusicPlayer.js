8958d3228d1a5e0038ae03e4ed58f00498c42cc3
Zach Banducci <zbducci@gmail.com>
2019-04-04 14:44:23 -0500

    Music Player initializes correctly now

--- src/hooks/useMusicPlayer.js
+++ src/hooks/useMusicPlayer.js
@@ -6,30 +6,26 @@ import useSpotifyPlayer from './useSpotifyPlayer'
 function useMusicPlayer({title, artist, album, uri, duration}, nextSong) {
   const [play, setPlay, elapsed] = useSpotifyPlayer(uri)
 
-  MusicControl.enableBackgroundMode(true)
+  useEffect(function updatePlayback() {
+    MusicControl.enableBackgroundMode(true)
 
-  // Control setup for music player
-  MusicControl.enableControl('play', true)
-  MusicControl.enableControl('pause', true)
-  MusicControl.enableControl('nextTrack', true)
-  MusicControl.enableControl('prevTrack', false)
+    MusicControl.setNowPlaying({
+      title,
+      artwork: album.url,
+      artist,
+      duration: Math.floor(duration/1000)
+    })
 
+    // Control setup for music player
+    MusicControl.enableControl('play', true)
+    MusicControl.enableControl('pause', true)
+    MusicControl.enableControl('nextTrack', true)
+    MusicControl.enableControl('prevTrack', false)
+  }, [uri])
+  
   MusicControl.on('play', () => setPlay(true))
   MusicControl.on('pause', () => setPlay(false))
   MusicControl.on('nextTrack', () => nextSong())
-
-  MusicControl.setNowPlaying({
-    title,
-    artwork: album.url,
-    artist,
-    duration: Math.floor(duration/1000)
-  })
-  
-  useEffect(function updatePlayhead() {
-    MusicControl.updatePlayback({
-      elapsed
-    })
-  }, [elapsed])
   
   return [play, setPlay, elapsed]
 }
