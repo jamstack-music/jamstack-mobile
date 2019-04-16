00e0f12f265cffeee187e9784cba01af80a27c80
Zach Banducci <zbducci@gmail.com>
2019-04-16 14:59:10 -0500

    Resolved import issue in SongItem

--- src/components/SongItem.js
+++ src/components/SongItem.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
 import { View, Text } from 'react-native'
 import { Icon } from 'react-native-elements'
-import AlbumCover from '../components/AlbumCover'
+import AlbumCover from './AlbumCover'
 import { Subscribe } from 'unstated'
 import { RoomContainer } from '../store/room'
 
