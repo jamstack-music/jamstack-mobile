6bb69f76329d009e11eb7ac981f4f381244f287a
Zach Banducci <zbducci@gmail.com>
2019-04-24 20:54:05 -0500

    Currently playing added to navigator

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -2,7 +2,6 @@ import React from 'react'
 
 import { 
   createAppContainer, 
-  createStackNavigator,
   createSwitchNavigator,
   createBottomTabNavigator
 } from 'react-navigation'
@@ -13,14 +12,16 @@ import Login from '../screens/Login'
 import Room from '../screens/Room'
 import Search from '../screens/Search'
 import Browse from '../screens/Browse'
+import CurrentPlaying from '../screens/CurrentPlaying'
 
 import CreateRoom from '../screens/Createroom'
 import StoreInit from '../navigation/StoreInit'
 import Auth from '../navigation/Auth'
 
 const RoomStack = createBottomTabNavigator({
-  Search,
   Room,
+  'Currently Playing': CurrentPlaying,
+  Search,
   Browse,
 }, {
   defaultNavigationOptions: ({navigation}) => ({
@@ -29,6 +30,9 @@ const RoomStack = createBottomTabNavigator({
       let iconName
       switch (routeName) {
       case 'Browse': 
+        iconName = 'md-bookmarks'
+        break
+      case 'Currently Playing':
         iconName = 'md-disc'
         break
       case 'Search':
@@ -37,11 +41,7 @@ const RoomStack = createBottomTabNavigator({
       case 'Room':
         iconName = 'md-home'
         break 
-      case 'CreateRoom':
-        iconName = 'md-add'
-        break
-    }
-
+      }
       return <Ionicons name={iconName} size={20} color={tintColor}/>
     }
   }),
