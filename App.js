e2523b53c69d70168677f40f24db6a78b1737726
Zachary Banducci <zbducci@gmail.com>
2020-01-04 21:25:08 -0800

    Added ReduxContainer and AuthProvider to App.js

--- App.js
+++ App.js
@@ -2,24 +2,25 @@ import React from 'react';
 import Spotify from 'rn-spotify-sdk';
 import { NavigationNativeContainer } from '@react-navigation/native';
 import { ReduxContainer } from 'jamstate';
+import AuthProvider from 'Components/Auth';
 
 import AppNavigation from './src/navigation';
 
 Spotify.initialize({
   clientID: '0a31a2abfc5945bb9e3b3507e6f8361c',
   sessionUserDefaultsKey: 'SpotifySession',
-  redirectURL: 'localhost:4000/authorize',
+  redirectURL: 'http://localhost:4000/v1/authenticate',
   scopes: ['streaming'],
 });
 
-const App = () => {
-  return (
-    <ReduxContainer>
+const App = () => (
+  <ReduxContainer>
+    <AuthProvider>
       <NavigationNativeContainer>
         <AppNavigation />
       </NavigationNativeContainer>
-    </ReduxContainer>
-  );
-};
+    </AuthProvider>
+  </ReduxContainer>
+);
 
 export default App;
