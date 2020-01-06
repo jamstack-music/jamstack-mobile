0ab9fd3944cd7636a57f8d9ad5b7e11d870183c9
Zachary Banducci <zbducci@gmail.com>
2020-01-05 18:57:55 -0800

    Fixed duplicate Spotify initialization

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Spotify from 'rn-spotify-sdk';
 import { NavigationNativeContainer } from '@react-navigation/native';
 import { ReduxContainer } from 'jamstate';
@@ -6,23 +6,35 @@ import AuthProvider from 'Components/Auth';
 
 import AppNavigation from './src/navigation';
 
-Spotify.initialize({
-  clientID: '0a31a2abfc5945bb9e3b3507e6f8361c',
-  sessionUserDefaultsKey: 'SpotifySession',
-  redirectURL: 'jamstack://auth',
-  scopes: ['streaming'],
-  tokenSwapURL: 'http://localhost:4000/v1/spotify/tokens/swap',
-  tokenRefreshURL: 'http://localhost:4000/v1/spotify/tokens/refresh',
-});
+const App = () => {
+  useEffect(() => {
+    async function initSpotify() {
+      const isInitialized = await Spotify.isInitializedAsync();
 
-const App = () => (
-  <ReduxContainer>
-    <AuthProvider>
-      <NavigationNativeContainer>
-        <AppNavigation />
-      </NavigationNativeContainer>
-    </AuthProvider>
-  </ReduxContainer>
-);
+      if (!isInitialized) {
+        Spotify.initialize({
+          clientID: '0a31a2abfc5945bb9e3b3507e6f8361c',
+          sessionUserDefaultsKey: 'SpotifySession',
+          redirectURL: 'jamstack://auth',
+          scopes: ['streaming'],
+          tokenSwapURL: 'http://localhost:4000/v1/spotify/tokens/swap',
+          tokenRefreshURL: 'http://localhost:4000/v1/spotify/tokens/refresh',
+        });
+      }
+    }
+    
+    initSpotify();
+  }, []);
+
+  return (
+    <ReduxContainer>
+      <AuthProvider>
+        <NavigationNativeContainer>
+          <AppNavigation />
+        </NavigationNativeContainer>
+      </AuthProvider>
+    </ReduxContainer>
+  );
+};
 
 export default App;
