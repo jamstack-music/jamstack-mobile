3daa60d7bdf362e9c048857fa05bdc6ec10ef5f5
Zachary Banducci <zbducci@gmail.com>
2020-01-05 18:58:57 -0800

    Fixed useInterval stale closure issue

--- src/hooks/useInterval.js
+++ src/hooks/useInterval.js
@@ -1,13 +1,24 @@
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useCallback } from 'react';
+
+function useEventCallback(fn) {
+  const ref = useRef(fn);
+
+  useEffect(() => {
+    ref.current = fn;
+  });
+
+  return useCallback((...args) => ref.current.apply(void 0, args), []);
+}
 
 export default function useInterval(callback, delay) {
-  const callbackRef = useRef(callback);
+  const savedCallback = useEventCallback(callback);
 
   useEffect(() => {
     function tick() {
-      callbackRef.current();
+      savedCallback();
     }
+
     const id = setInterval(tick, delay);
     return () => clearInterval(id);
-  }, [delay]);
+  }, [delay, savedCallback]);
 }
