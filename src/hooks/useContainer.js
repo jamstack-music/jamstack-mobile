619ee9894841e1153faac8eda00f8b3ff586489b
Zachary Banducci <zbducci@gmail.com>
2020-01-06 02:39:23 -0800

    Added container hook and store creation

---
+++ src/hooks/useContainer.js
@@ -0,0 +1,26 @@
+import React, { createContext, useContext } from 'react';
+
+// TODO: Add docs
+export function createContainer(useHook, displayName = null) {
+  const Context = createContext(null);
+  if (displayName) {
+    Context.displayName = displayName;
+  }
+
+  function Provider(props) {
+    const { value, children } = props;
+    const contextValue = useHook(value);
+
+    return <Context.Provider value={contextValue}>{children}</Context.Provider>;
+  }
+
+  return {
+    useContainer: () => useContext(Context),
+    Provider,
+    Context,
+  };
+}
+
+export function useContainer(Container) {
+  return useContext(Container);
+}
