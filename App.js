2e336d610d69b994e9ef3f8918909b0d989b52c5
Zachary Banducci <zbducci@gmail.com>
2020-01-20 21:21:53 -0600

    Sentry SDK installed and linked

--- App.js
+++ App.js
@@ -3,7 +3,12 @@ import { NavigationNativeContainer } from '@react-navigation/native';
 import { ReduxContainer } from 'jamstate';
 import AuthContainer from 'Containers/Auth';
 
+import * as Sentry from '@sentry/react-native';
 import AppNavigation from './src/navigation';
+
+Sentry.init({
+  dsn: 'https://75baf38f4b2d41afa274f54acbea3469@sentry.io/1889339',
+});
 
 const App = () => (
   <ReduxContainer>
