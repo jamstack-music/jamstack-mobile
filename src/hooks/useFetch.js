bef2d9f346a866826531de3266400bd39789ece4
Zachary Banducci <zbducci@gmail.com>
2020-01-26 15:21:52 -0600

    Fixed hooks

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,17 +1,44 @@
-import { useState, useCallback } from 'react';
+import { useReducer, useCallback } from 'react';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'LOAD':
+      return {
+        data: null,
+        error: null,
+        isLoading: true,
+      };
+    case 'DATA':
+      return {
+        ...state,
+        data: action.payload,
+        isLoading: false,
+      };
+    case 'ERROR': {
+      return {
+        ...state,
+        error: action.payload,
+        isLoading: false,
+      };
+    }
+    default:
+      return state;
+  }
+};
 
 export default function useFetch() {
-  const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
+  const [state, dispatch] = useReducer(reducer, {
+    isLoading: false,
+    error: null,
+    data: null,
+  });
 
   const fetch = useCallback((api, ...args) => {
-    setIsLoading(true);
-
+    dispatch({ type: 'LOAD' });
     return api(...args)
-      .then(result => result)
-      .catch(e => setError(e))
-      .finally(() => setIsLoading(false));
+      .then(result => dispatch({ type: 'DATA', payload: result }))
+      .catch(e => dispatch({ type: 'ERROR', payload: e }));
   }, []);
 
-  return { error, isLoading, fetch };
+  return { ...state, fetch };
 }
